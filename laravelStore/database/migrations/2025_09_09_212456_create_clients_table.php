5868199a9c0dd81ab06360d02e64590a81fa0f54
DallmannAr <arturdallmann7@gmail.com>
2025-09-09 18:45:13 -0300

    feature: new migrations added to the database folder

---
+++ laravelStore/database/migrations/2025_09_09_212456_create_clients_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('clients_name');
+            $table->string('clients_cpf')->unique();
+            $table->string('clients_telephone')->nullable();
+            $table->string('clients_email')->isNotEmpty();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
